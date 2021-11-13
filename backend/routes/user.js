42f7da77a46f8290c0729cfc30b11b1f800ab6e4
FullStackUp <fullstackup.webdeveloper@gmail.com>
2021-11-13 18:17:22 +0100

    update router (user)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 //express permet de creer un router
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const userCtrl = require('../controllers/user');
+const validate = require('../middleware/validate-inputs');
+
+router.post('/signup', validate.user, userCtrl.signup);
+router.post('/login', validate.user, userCtrl.login);
 
 module.exports = router;
